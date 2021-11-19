4405b0317bc16f4db8c60b816a2ff93fa47e640a
Ilya Lekomcev <lekomtzev.ilja@gmail.com>
2021-11-19 13:13:47 +0500

    Переименует функцию

--- project/src/components/reviews-form/reviews-form.tsx
+++ project/src/components/reviews-form/reviews-form.tsx
@@ -29,7 +29,7 @@ function ReviewsForm({ id }: reviewsFormPropsTypes): JSX.Element {
     setMessage('');
   };
 
-  const checkButtonDisbled = () => {
+  const handleButtonDisabled = () => {
     if (rating > 0 && message.length >= MESSAGE_MIN_LENGTH && message.length <= MESSAGE_MAX_LENGTH) {
       return false;
     } else {
@@ -55,7 +55,7 @@ function ReviewsForm({ id }: reviewsFormPropsTypes): JSX.Element {
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
         </p>
-        <button className="reviews__submit form__submit button" type="submit" disabled={checkButtonDisbled()}>Submit</button>
+        <button className="reviews__submit form__submit button" type="submit" disabled={handleButtonDisabled()}>Submit</button>
       </div>
     </form>
   );
